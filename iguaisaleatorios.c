50c4b9c089582a9b614ea14542bb9d041c4502ea
thiaguinho-dev01 <thigas1105@gmail.com>
2026-04-13 15:22:04 -0300

    Add files via upload

---
+++ iguaisaleatorios.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+    srand(time(NULL));
+
+    int a, b;
+    int tentativas = 0;
+
+    do
+    {
+        a = 1 + rand() % 1000;
+        b = 1 + rand() % 1000;
+        tentativas++;
+    } while (a != b);
+
+    printf("Após %d tentativas, os dois números gerados são iguais a %d\n", tentativas, a);
+
+    printf("Criado por [SEU NOME]");
+}
